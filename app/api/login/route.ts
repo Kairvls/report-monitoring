eae8c1b56f783b0c688fa2f99966b2920ca47669
JC Suan <jhoncarlosuan26@gmail.com>
2026-04-20 00:51:42 +0800

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
 
   const token = jwt.sign(
     { id: user.id },
-    "secret_key_reports_monitoring",
+    process.env.JWT_SECRET!,
     { expiresIn: "1d" }
   );
 

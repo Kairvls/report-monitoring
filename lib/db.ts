c7cc8cc3212647fac9350861b3f3109f54ae8613
JC Suan <jhoncarlosuan26@gmail.com>
2026-04-20 00:54:39 +0800

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,21 @@
 import mysql from "mysql2/promise";
 
 declare global {
-  // allow global var in dev (important for Next.js hot reload)
   var _dbPool: mysql.Pool | undefined;
 }
 
 export const db =
   global._dbPool ||
   mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "kaisuan", // change if you have password
-    database: "reports_monitoring", // ⚠️ change this
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10, // prevents "too many connections"
+    connectionLimit: 10,
     queueLimit: 0,
   });
 
-// ✅ prevent multiple pools in dev (VERY IMPORTANT)
 if (process.env.NODE_ENV !== "production") {
   global._dbPool = db;
 }

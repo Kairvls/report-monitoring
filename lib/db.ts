4fb0bafd31aab2c35dadc82c8ef550d28878ca70
JC Suan <jhoncarlosuan26@gmail.com>
2026-04-20 01:41:07 +0800

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -6,15 +6,7 @@ declare global {
 
 export const db =
   global._dbPool ||
-  mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
+  mysql.createPool(process.env.MYSQL_URL!);
 
 if (process.env.NODE_ENV !== "production") {
   global._dbPool = db;

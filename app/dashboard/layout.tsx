aaa0a8c57579aeb76b6b0e7965988e3a5b3a9003
JC Suan <jhoncarlosuan26@gmail.com>
2026-04-21 00:53:01 +0800

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -314,7 +314,7 @@ export default function DashboardLayout({
                   xmlns="http://www.w3.org/2000/svg"
                   className="w-5 h-5"
                   viewBox="0 0 24 24"
-                  fill="none"
+                  fill="black"
                   stroke="currentColor"
                   strokeWidth="2"
                 >

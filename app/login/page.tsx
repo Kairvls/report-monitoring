a1438ee467178f6d041e5be7a4dda2a09334c076
JC Suan <jhoncarlosuan26@gmail.com>
2026-04-21 00:39:13 +0800

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -155,7 +155,7 @@ export default function Login() {
                 ${
                   isDisabled
                     ? "cursor-not-allowed bg-black/10 opacity-50"
-                    : "bg-black text-slate-950 shadow-lg shadow-black/20 hover:bg-slate-100"
+                    : "bg-black text-slate-950 shadow-lg shadow-black/20 hover:bg-gray-600"
                 }`}
             >
               {!loading && <ShieldCheck size={18} />}
